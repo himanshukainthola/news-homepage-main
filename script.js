83b23f62b875996d279e67fe60336977b3138dab
Himanshu Kainthola <hkainthola6@gmail.com>
2023-03-04 19:11:39 +0530

    edit navigation menu and button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const navIcons = document.querySelector('.navbar__icons');
 const navMenu = document.querySelector('.nav__menu');
-
+const body = document.querySelector('body');
 
 navIcons.addEventListener('click', () => {
     const visibilty = navMenu.getAttribute('data-visible');
@@ -8,8 +8,10 @@ navIcons.addEventListener('click', () => {
     if (visibilty === "false") {
         navMenu.setAttribute('data-visible', 'true');
         navIcons.setAttribute('aria-expanded', 'true');
+        body.classList.add('show');
     }else{
         navMenu.setAttribute('data-visible', 'false');
         navIcons.setAttribute('aria-expanded', 'false');
+        body.classList.remove('show');
     }   
 });
